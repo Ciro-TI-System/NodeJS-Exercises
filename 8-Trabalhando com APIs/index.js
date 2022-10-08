58b2efafd5a131da73a10efe79bfba546c999acf
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2022-10-08 00:02:09 -0300

    Executing 'POST' in API
    Executing the 'POST' method on the API using the 'Insomnia' API client.

--- 8-Trabalhando com APIs/index.js
+++ 8-Trabalhando com APIs/index.js
@@ -4,5 +4,13 @@ const app = express()
 
 app.listen('3000')
 
-app.route('/').get((req, res) => res.send('Hello, '))
-app.route('/sobre').get((req, res) => res.send('Hello, sobre'))
+//middleware
+app.use(express.json())
+
+//GET
+//app.route('/').get((req, res) => res.send('Hello, '))
+//app.route('/sobre').get((req, res) => res.send('Hello, sobre'))
+
+//POST
+//app.route('/').post((req, res) => console.log(req.body))
+app.route('/').post((req, res) => res.send(req.body))
