7bdfec86c9ffb1aeb15813daaec3b2b12b19c57f
Ciro Batista da Silva <cirobatista.cbs2020ti@outlook.com>
2022-10-10 11:38:25 -0300

    Performing a GET on the 'API' Node.

--- 10-API com Fetch/main.js
+++ 10-API com Fetch/main.js
@@ -2,7 +2,11 @@ const url = 'http://localhost:5500/api'
 
 function getUsers() {
   fetch(url)
-    .then(response => console.log(response.json()))
+    /*.then(function (response) {
+      return response.json()
+    })*/
+    .then(response => response.json()) //Anonymous Function
+    .then(data => renderApiResult.textContent = JSON.stringify(data))
     .catch(error => console.error(error))
 }
 
